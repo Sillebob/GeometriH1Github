991a8622b5bbfc7c2887c310cbd7253757b66cfd
Sillebob <78139966+Sillebob@users.noreply.github.com>
2021-02-22 12:01:10 +0100

    Add project files.

---
+++ GeometriH1Github/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GeometriH1Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //omkreds
+            Square kvadrat1 = new Square(2.54); //instantierer et nyt objekt af klassen Square
+            Console.WriteLine(Math.Round(kvadrat1.Getperimeter(), 2));//Udskriver og kalder metoden til at beregne omkreds og afrunder til 2 decimaler;
+
+            Square kvadrat2 = new Square(5.25);//instatierer et nyt objekt af klasssen Square med en anden side værdi
+            Console.WriteLine(Math.Round(kvadrat2.Getperimeter(), 2)); /*Udskriver og kalder metoden til at beregne omkreds af det nye objekt
+                                                                          * og afrunder til 2 decimaler*/
+
+            Square kvadrat3 = new Square(15.00);//instatierer et nyt objekt af klasssen Square med en anden side værdi
+            Console.WriteLine(Math.Round(kvadrat3.Getperimeter(), 2)); /*Udskriver og kalder metoden til at beregne omkreds af det tredje objekt
+                                                                          * og afrunder til 2 decimaler*/
+
+            //Areal
+            Console.WriteLine(Math.Round(kvadrat1.Getareal(), 2));/*Udskriver og kalder metoden til at beregne areal af det første objekt
+                                                                    * og afrunder til 2 decimaler*/
+
+            Console.WriteLine(Math.Round(kvadrat2.Getareal(), 2)); /* Udskriver og kalder metoden til at beregne areal af det andet objekt
+                                                                      * og afrunder til 2 decimaler*/
+            Console.WriteLine(Math.Round(kvadrat3.Getareal(), 2)); /* Udskriver og kalder metoden til at beregne areal af det tredje objekt
+                                                                      * og afrunder til 2 decimaler*/
+
+            // Opgave B
+            Square setnewvalue = new Square(25); //instantierer et nyt objekt af klassen Square med værdien af variablen side = 5
+
+            Console.WriteLine(setnewvalue.Getperimeter());
+
+            setnewvalue.Side = 12; //Ændrer nu værdien til 12
+
+            Console.WriteLine(setnewvalue.Getperimeter()); //som det ses af udskriften er der nu brugt den nye værdi til udregning af omkredsen
+
+            Square setnewvalue2 = new Square(5); //instantierer et nyt objekt af klassen Square med værdien af variablen side = 5
+
+            Console.WriteLine(setnewvalue2.Getareal());
+
+            setnewvalue2.Side = 7; //Ændrer nu værdien til 7
+
+            Console.WriteLine(setnewvalue2.Getareal()); //som det ses af udskriften er der nu brugt den nye værdi til udregning af arealet
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ GeometriH1Github/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometriH1Github
+{
+    class Square
+    { 
+   private double side; //Erklærer en variabel
+
+    public Square(double side) //opretter en konstruktør der har en egenskab defineret som side
+    {
+        this.side = side; //definerer at side i classen skal være det samme som siden konstruktøren tager med fra Main
+
+    }
+
+    //Opgave b
+
+    public Square() //Opretter en tom konstruktør
+    {
+    }
+    public double Side //Implementerer get & set properties til at bestemme hvad der må gøres ved variablen side
+    {
+        get
+        {
+            return this.side; // Alle kan se variablen side
+        }
+        set
+        {
+            this.side = value; //Alle kan sætte værdien for variablen
+        }
+
+    }
+    //Metoder
+
+    //omkreds
+    public double Getperimeter()
+    {
+        double resultat = side * 4; /*da det er et kvadrat har siderne alle den samme værdi 
+                                               * og man behøver derfor ikke lægge alle siderne sammen.*/
+        return resultat;
+
+    }
+    //areal
+    public double Getareal()
+    {
+        double resultat = side * side;
+        return resultat;
+    }
+
+}
+
+}
